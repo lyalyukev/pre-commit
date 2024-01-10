d9151063b504ed6d3c9cb624c165bd5a48888596
lyalyukev <lyalyuk.ev@gmail.com>
2024-01-10 15:32:14 +0200

    Install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -d .git ]; then
+    
+    # Download Gitleaks as submodule
+    git submodule add https://github.com/zricethezav/gitleaks.git gitleaks
+    
+    # Add submodule to .gitignore    
+    echo /gitleaks > .gitignore
+
+    # Build binary file gitleaks for our os and arch
+    make build
+
+    # Download git pre-commit hook
+    wget -P .git/hooks https://raw.githubusercontent.com/lyalyukev/precommit/main/pre-commit
+
+    # Make hook executable
+    chmod +x .git/hooks/pre-commit
+
+    # Set git option gitleaks.enable
+    git config --local gitleaks.enable true
+    
+else
+    echo "You should init git repo before install Gitleaks"
+    exit 1 
+fi
